50667e3735069ed182804f2f3c8904dc8225869f
kirill <kirill.shakhov14@gmail.com>
2020-12-08 06:22:23 +0300

    fix

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-scp -P 2222 target\lab3-1.0-SNAPSHOT.war s282927@helios.se.ifmo.ru:~/wildfly/standalone/deployments/
+scp -P 2222 target\lab3-1.0-SNAPSHOT.war s285896@helios.se.ifmo.ru:~/wildfly/standalone/deployments/
